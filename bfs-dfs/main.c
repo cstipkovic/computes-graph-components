a4d00b569607817f1c90d055b49ea7aaf0b89379
Clauber Stipkovic <clauber.halic@gmail.com>
2019-05-13 13:13:59 -0300

    Adding makefile and main c for new homework

---
+++ bfs-dfs/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct e {
+
+} Edge;
+
+typedef struct v {
+    int name;
+    int component;
+    Edge *prim;
+} Vertex;
+
+void BFS(G, s) {
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+}
+
+void DFS() {
+
+}
+
+int main () {
+    return 0;
+}
